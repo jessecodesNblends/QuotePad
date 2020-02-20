3d1170e54b5a6f0679c47ed293aade046b456425
jessecodesNblends <ocran.jss@gmail.com>
2020-02-20 05:42:26 +0000

    Add Header.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
 	<div class="container">
-		<app-new-quote></app-new-quote>
-		<appQuoteGrid v-bind:quotes='quotes'></appQuoteGrid>
+		<app-header></app-header>
+		<app-new-quote v-on:quoteAdded='newQuote'></app-new-quote>
+		<appQuoteGrid v-bind:quotes='quotes' v-on:quoteDeleted='deleteQuote'></appQuoteGrid>
+		<div class="row">
+			<div class="col-sm-12 text-center">
+				<div class="alert alert-info">Attention!: Select a Quote to delete</div>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
 import QuoteGrid from './components/QuoteGrid.vue'
 import NewQuote from './components/NewQuote.vue'
+import Header from './components/Header.vue'
 
 export default {
 	components: {
 		appQuoteGrid: QuoteGrid,
-		appNewQuote: NewQuote
+		appNewQuote: NewQuote,
+		appHeader: Header,
 	},
 	data: function() {
 		return {
@@ -22,5 +30,13 @@ export default {
 			maxQuotes: 10,
 		}	
 	},
+	methods: {
+		newQuote(quote) {
+			this.quotes.push(quote);
+		},
+		deleteQuote(index) {
+			this.quotes.splice(index, 1)
+		}
+	}
 };
 </script>
